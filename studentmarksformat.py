703d3ad3b55a0f1d0eea1c7a376ebd4555100992
Mohammed Sabiq <mohamedsabiq78@gmail.com>
2026-02-01 02:58:34 -0800

    backup

---
+++ studentmarksformat.py
@@ -0,0 +1,17 @@
+class studentmarks:
+    marks = 76
+    if marks >= 90:
+        grade = 'A'
+    elif marks >= 80:
+        grade = 'A+'
+    elif marks >= 70:
+        grade = 'B'
+    elif marks >= 60:
+        grade = 'B+'
+    elif marks >= 50:
+        grade = 'C'
+    elif marks >= 40:
+        grade = 'D'
+    else:
+        grade = 'F'
+    
